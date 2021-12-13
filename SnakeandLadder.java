921cae1454ea6c1c85b578787d877adc47dd3f78
Lalitha-12 <73027128+Lalitha-12@users.noreply.github.com>
2021-12-13 12:24:27 +0530

    Create SnakeandLadder.java

---
+++ SnakeandLadder.java
@@ -0,0 +1,17 @@
+package lldoops;
+public class SnakeandLadder {
+	  private int start;
+	  private int end;
+	  SnakeandLadder(int start,int end){
+	      this.start=start;
+	      this.end=end;
+	  }
+	  public SnakeandLadder(int nextInt, String nextLine) {
+	}
+	public int getStart(){
+	      return start;
+	  }
+	  public int getEnd(){
+	      return end;
+	  }
+	}
